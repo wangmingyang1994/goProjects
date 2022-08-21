dac9d0f1a9f071698ffff1c7d16ac67e3e197bf7
mingyang <15010029361@163.com>
2022-08-21 22:12:13 +0800

    add some mark

--- studyForGolang/1st/main.go
+++ studyForGolang/1st/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-"context"
-"flag"
-"log"
-"net/http"
-"os"
-"os/signal"
-"time"
+	"context"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 var (
@@ -22,35 +22,38 @@ func main() {
 	done := make(chan bool, 1)
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
-	// 开启
 	server := newWebserver(logger)
 	go gracefulShutdown(server, logger, quit, done)
-
 	logger.Println("Server is ready to handle requests at", listenAddr)
 	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		logger.Fatalf("Could not listen on %s: %v\n", listenAddr, err)
 	}
-
+	// 阻塞，接收关闭channel的信号
 	<-done
 	logger.Println("Server stopped")
 }
 
 func gracefulShutdown(server *http.Server, logger *log.Logger, quit <-chan os.Signal, done chan<- bool) {
+	// 接收ctrl+c的信号
 	<-quit
 	logger.Println("Server is shutting down...")
+	//创建context，设置10秒超时
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	server.SetKeepAlivesEnabled(false)
+	// 刷入缓存
 	time.Sleep(time.Second * 2)
 	log.Println("Brush the cache success~")
 	time.Sleep(time.Second * 2)
 	if err := server.Shutdown(ctx); err != nil {
 		logger.Fatalf("Could not gracefully shutdown the server: %v\n", err)
 	}
+	//关闭channel
 	close(done)
 }
 
 func newWebserver(logger *log.Logger) *http.Server {
+	// 注册一个server
 	router := http.NewServeMux()
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
